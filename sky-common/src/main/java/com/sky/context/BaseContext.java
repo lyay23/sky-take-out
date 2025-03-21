f7d4c5ff25e9aba769c86c3ea74763884d3633d0
lyay23 <3050681193@qq.com>
2025-03-21 21:18:33 +0800

    新增员工业务代码开发--使用ThreadLocal来设置数据库的操作员，完成了控制台重名的捕获

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,17 +1,18 @@
 package com.sky.context;
 
 public class BaseContext {
-
+    // 当前线程的id
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    // 设置当前线程的id
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
-
+    // 获取当前线程的id
     public static Long getCurrentId() {
         return threadLocal.get();
     }
-
+    // 移除当前线程的id
     public static void removeCurrentId() {
         threadLocal.remove();
     }
