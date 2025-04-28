bf50ab18d573c0dacb33de33c7ab4b876b250777
lyay23 <3050681193@qq.com>
2025-04-28 11:46:57 +0800

    用户界面小程序端-使用SpringCache缓存套餐功能

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -11,7 +11,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.Cacheable;
 
 import java.util.List;
 
@@ -35,6 +37,12 @@ public class SetmealController {
      */
     @ApiOperation(value = "新增套餐")
     @PostMapping
+
+    // 删除redis的缓存数据
+    //这里为什么要删除而不是新增redis呢？
+    // 因为新增和修改的逻辑是一样的，所以这里我们使用删除缓存数据，而不是新增缓存数据
+    // 新增时我们需要清理旧的缓存，不然会覆盖掉新增的缓存数据
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO) {
         log.info("新增套餐:{}", setmealDTO);
         setmealService.save(setmealDTO);
@@ -59,6 +67,7 @@ public class SetmealController {
      */
     @ApiOperation(value = "删除套餐")
     @DeleteMapping
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids)  {
         log.info("删除套餐:{}", ids);
         setmealService.deleteBatch(ids);
@@ -72,6 +81,7 @@ public class SetmealController {
     //根据id查询套餐
     @ApiOperation(value = "根据id查询套餐")
     @GetMapping("/{id}")
+
     public Result<SetmealVO> getById(@PathVariable Long id)  {
         log.info("根据id查询套餐:{}", id);
         SetmealVO setmealVO = setmealService.getByIdWithCatory(id);
@@ -80,6 +90,7 @@ public class SetmealController {
     //修改套餐
     @PutMapping
     @ApiOperation(value = "修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO)  {
         log.info("修改套餐:{}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -89,6 +100,7 @@ public class SetmealController {
     // 套餐启用禁用功能
     @PostMapping("/status/{status}")
     @ApiOperation(value = "套餐启用禁用功能")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop( @PathVariable Integer status,Long id)  {
         log.info("套餐启用禁用功能:{}", id);
         setmealService.updateStatus(status,id);
